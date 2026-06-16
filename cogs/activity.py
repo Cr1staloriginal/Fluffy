1eea0c8721baa7e93c144338e62e8910b8eab789
Cr1stalik ˢˢ <dmitrijosipov877@gmail.com>
2026-06-17 00:07:31 +0500

    Create activity.py

---
+++ cogs/activity.py
@@ -0,0 +1,54 @@
+import disnake
+from disnake.ext import commands
+import time
+from database import increment_messages, add_voice_minutes, set_voice_join_time, get_voice_join_time, add_cookies
+
+class Activity(commands.Cog):
+    def __init__(self, bot: commands.InteractionBot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: disnake.Message):
+        if message.author.bot or message.guild is None:
+            return
+        await increment_messages(message.author.id)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: disnake.Member, before: disnake.VoiceState, after: disnake.VoiceState):
+        if member.bot:
+            return
+        if before.channel is None and after.channel is not None:
+            await set_voice_join_time(member.id, time.time())
+        elif before.channel is not None and after.channel is None:
+            join_time = await get_voice_join_time(member.id)
+            if join_time:
+                seconds = int(time.time() - join_time)
+                minutes = seconds // 60
+                if minutes > 0:
+                    await add_voice_minutes(member.id, minutes)
+                await set_voice_join_time(member.id, None)
+        elif before.channel is not None and after.channel is not None and before.channel != after.channel:
+            join_time = await get_voice_join_time(member.id)
+            if join_time:
+                seconds = int(time.time() - join_time)
+                minutes = seconds // 60
+                if minutes > 0:
+                    await add_voice_minutes(member.id, minutes)
+            await set_voice_join_time(member.id, time.time())
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: disnake.RawReactionActionEvent):
+        if payload.user_id == self.bot.user.id:
+            return
+        if str(payload.emoji) == "🍪":
+            channel = self.bot.get_channel(payload.channel_id)
+            if channel:
+                try:
+                    message = await channel.fetch_message(payload.message_id)
+                    if message.author and not message.author.bot:
+                        await add_cookies(message.author.id)
+                except:
+                    pass
+
+def setup(bot: commands.InteractionBot):
+    bot.add_cog(Activity(bot))
